c61bcc1c663528797f64439e056c54bccdd505ff
Felipe Rabuffetti <rabuffetti93@gmail.com>
2020-06-06 13:31:39 -0300

    agregue el onclick en productos

--- js/script.js
+++ js/script.js
@@ -1,28 +1,49 @@
-// capturamos el formulario
-const navItems = document.querySelector('.mynavbar-item');
+
+
+function toggle_visibility(id) {
+  let e = document.getElementById(id);
+  if(e.style.display == 'block')
+     e.style.display = 'none';
+  else
+     e.style.display = 'block';
+}
+
+function hide_visibility(id) {
+  let e = document.getElementById(id);
+     e.style.display = 'none';
+}
+
 
 /*
-// capturar los elementos del formulario en un array
-//const formInputs = Array.from(form.elements);
+const product1 = document.getElementById('product1');
+const productTable1 = document.getElementById('productTable1');
+product1.addEventListener('click', function () {
 
-// ================== Recorre los campos del formulario y valida cada uno ==================
- formInputs.forEach(oneInput => {
-     
-     oneInput.addEventListener('blur', function () {
+  console.log(productTable1.style.display)
 
-            this.classList.add('form-error');
-           
-         }
-     )
-
- }); //Cierre del forEach
- */
+    if (productTable1.style.display === "none") {
+        console.log('HOLA')
+        productTable1.classList.add('product-table-on')
+      } else {
+        console.log('CHAU')
+        productTable1.classList.remove('product-table-on')
+      }
 
 
-navItems.addEventListener('hover', function () {
-    navItems.classList.add('prueba1')
-    
 })
 
+const product2 = document.getElementById('product2');
+const productTable2 = document.getElementById('productTable2');
+product2.addEventListener('click', function () {
+    productTable2.classList.add('product-table-on')
+})
+
+const product3 = document.getElementById('product3');
+const productTable3 = document.getElementById('productTable3');
+product3.addEventListener('click', function () {
+    productTable3.classList.add('product-table-on')
+})
+
+*/
 
 
